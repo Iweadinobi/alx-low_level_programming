da6447888c2bea8bfbae751bdc3135bac45e8e38
Iweadinobi <iweadinobi22@gmail.com>
2022-12-08 21:07:39 -0800

    C 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
- */
-int main(void) {
-  puts("\" Programming is like building a multilingual puzzle, followed by a new line.");
-  return (0);
+* main - Entry
+* Return: Always 0
+*/
+int main(void)
+{
+puts("\" Programming is like building a multilingual puzzle");
+return (0);
 }
