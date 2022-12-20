af39dd4bf62fb11e529b7702e668c156e0f64cc6
Iweadinobi <iweadinobi22@gmail.com>
2022-12-20 01:57:30 -0800

    CV 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
 * main - Entry point
-* Return - Always 0 (Success)
+*
+* Return: Always 0 (Success)
 */
-int main(void)  
+int main(void)
 {
-int n;
-  
-      
+char c;
+c = 'a';
+while
+(c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
