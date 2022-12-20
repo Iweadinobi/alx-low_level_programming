fc8bb93cfd9814d253d52b1e8d878ca03fe89298
Iweadinobi <iweadinobi22@gmail.com>
2022-12-20 07:30:12 -0800

    CV 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- *
- *
- *
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-int ;
+int k, w;
+for (k = '0'; k < '9'; k++)
+{
+for (w = k + 1; w <= '9'; w++)
+{
+if (w != k)
+{
+putchar(k);
+putchar(w);
+if (k == '8' && w == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
