18e1ecd85313b3aa5614081507a113059aca4f91
Iweadinobi <iweadinobi22@gmail.com>
2022-12-20 11:33:24 -0800

    CV 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
 * main - Entry point
 *
@@ -9,22 +7,19 @@
 int main(void)
 {
 int d, p, q;
-for (d = '0'; d < '9'; d++)
+for (d = 0; d < 8; d++)
 {
-for (p = d + 1; p <= '9'; p++)
+for (p = d + 1; p <= 9; p++)
 {
-for (q = p + 1; q <= '9'; q++)
+for (q = p + 1; q <= 10; q++)
 {
-if ((p != d) != q)
-{
-putchar(d);
-putchar(p);
-putchar(q);
-if (d == '7' && p == '8')
+putchar((d % 10) + '0');
+putchar((p % 10) + '0');
+putchar((q % 10) + '0');
+if (d == '7' && p == '8' && q == '9')
 continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
