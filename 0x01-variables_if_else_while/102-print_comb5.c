8f43ac0694dcf7affcdc3a7060cd59079262858c
Iweadinobi <iweadinobi22@gmail.com>
2022-12-20 10:53:47 -0800

    CV 13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
 * main - Entry point
 *
@@ -6,7 +8,22 @@
 */
 int main(void)
 {
-  int ;
+int l, m;
+for (l = 0; l <= 98; l++)
+{
+for (m = l + 1; m <= 99; m++)
+{
+putchar((l / 10) + '0')
+putchar((l % 10) + '0');
+putchar(' ');
+putchar((m / 10) + '0');
+putchar((m % 10) + '0');
+if (l == 98 && m == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n')
 return (0);
