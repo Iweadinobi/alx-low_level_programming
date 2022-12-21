04a981590ffeb91ad6c34a8874c3a909d7307215
Iweadinobi <iweadinobi22@gmail.com>
2022-12-20 20:21:20 -0800

    CF 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+* _add - 
+* @:
+* Return: 
+*/
+int
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,35 @@
 #include "main.h"
 /**
-* 
-* @: 
-* Return: 
+* times_table - prints the 9 times tables, starting from 0
+* Return: empty output
 */
-int 
+void times_table(void)
+{
+int g, h, i, j, k;
+for (g = 0; g <= 9; g++)
+{
+for (h = 0; h <= 9; h++)
+{
+i = g * h;
+if (i > 9)
+{
+j = i % 10;
+k = (i - j) / 10;
+_putchar(&&);
+_putchar(32);
+_putchar(k + '0')
+_putchar(j + '0');
+}
+else
+{
+if (h != 0)
+{
+_putchar(32);
+_putchar(32);
+}
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
+}
