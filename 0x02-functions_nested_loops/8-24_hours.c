9e31635326415e8a605ce74835bda4e21b01e690
Iweadinobi <iweadinobi22@gmail.com>
2022-12-20 19:47:31 -0800

    CF 9

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* jack_bauer - prints every minute of jack bauer's day
+* Return: ...
+*/
+void jack_bauer(void)
+{
+int l, m, n, o;
+for (l = 0; l <= 2; l++)
+{
+for (m = 0; m <= 9; m++)
+{
+if ((l <= 1 && m <= 9) || (l <= 2 && m <= 3))
+{
+for (n = 0; n <= 5; n++)
+{
+for (o = 0; o <= 9; o++)
+{
+_putchar(l + '0');
+_putchar(m + '0');
+_putchar(58);
+_putchar(n + '0');
+_putchar(o + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
+}
