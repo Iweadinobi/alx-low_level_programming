4992b3fcdaa10efb8c699210c18445b3b11d9edb
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 09:27:34 -0800

    MCF 7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,22 @@
 #include "main.h"
 /**
-* print_line - 
-* @: 
-* Return: 
+* print_line - draws a straight line
+* @n: the number of lines to draw
+* Return: empty
 */
+void print_line(int n)
+{
+int e;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (e = 0; e < n; e++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
