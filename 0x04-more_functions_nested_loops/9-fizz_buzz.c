7afd9c55c3de5abb0c61ba2feac1e1901f655ae7
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 10:21:13 -0800

    MCF 10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* fizz_buzz - 
-* @: 
-* Return: 
+* main - prints a fizz buzz program
+* Return: Always 0 (Success)
 */
+int main(void)
+{
+int b;
+for (b = 1; b <= 100; b++)
+{
+if ((b % 3 == 0) && (b % 5 == 0))
+{
+printf("fizzbuzz");
+}
+else if (b % 3 == 0)
+{
+printf("fizz");
+}
+else if (b % 5 == 0)
+{
+printf("buzz");
+}
+else
+{
+printf("%d", b);
+}
+if ((b != 100))
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
