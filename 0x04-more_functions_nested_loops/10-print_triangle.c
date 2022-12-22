cee9412736ef2e1f1eaed03a5b27092b3ce814e6
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 10:39:02 -0800

    MCF 11

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,29 @@
 #include "main.h"
 /**
-* print_triangle - 
-* @: 
-* Return: 
+* print_triangle - prints a triangle of squares
+* @size: the size of square triangles
+* Return: empty
 */
+void print_triangle(int size)
+{
+int v, w, x;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (v = 0; v < size; v++)
+{
+for (w = size - v; w > 1; w--)
+{
+_putchar(32);
+}
+for (x = 0; x <= v; x++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
