d3919385c766ecae2135abf29679de81630a9818
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 07:45:08 -0800

    MCF 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - check if a letter is upper case
+* @g: the letter to be checked
+* Return: 1 for upper case letter, 0 for others
+*/
+int _isupper(int g)
+{
+if (g >= 65 && g <= 90)
+{
+return (1);
+}
+return (0);
+}
