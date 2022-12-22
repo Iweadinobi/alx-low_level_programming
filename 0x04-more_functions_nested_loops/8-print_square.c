9412a4f98a0d697407bf8d79ed12f2a0d673a346
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 09:56:55 -0800

    MCF 9

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,25 @@
 #include "main.h"
 /**
-* print_square - 
-* @: 
-* Return: 
+* print_square - prints n squares, n number of times
+* @size: number of squares/number of times
+* Return: empty
 */
+void print_square(int size)
+{
+int q, r;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (q = 0; q < size; q++)
+{
+for (r = 0; r < size; r++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
