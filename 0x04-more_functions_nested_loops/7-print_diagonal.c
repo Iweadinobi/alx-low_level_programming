ecff7f38adbdc520df01a36b7b7bc1e99e7e4cf7
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 09:40:45 -0800

    MCF 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,26 @@
 #include "main.h"
 /**
-* print_diagonal - 
-* @: 
-* Return: 
+* print_diagonal - draws diagonal lines
+* @n: number of times to print diagonal lines
+* Return: empty
 */
+void print_diagonal(int n)
+{
+int f, h;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (f = 0; f < n; f++)
+{
+for (h = 0; h < f; h++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
