399f95688b895a00082dad50b9f16e1194dc987b
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 11:55:52 -0800

    CP 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* reset_to_98 - uodate the value of n to 98
+* reset_to_98 - update the value of n to 98
 * @n: input
 * Return: n
 */
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 void swap _int(int *a, int *b);
 int _strlen(char *s);
@@ -9,4 +12,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+
 #endif
