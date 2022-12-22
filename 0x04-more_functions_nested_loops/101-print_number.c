94541aecc732ce557c7c2f5c1c059539dfcfa352
Iweadinobi <iweadinobi22@gmail.com>
2022-12-22 11:17:07 -0800

    MCF 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,22 @@
-#include
+#include "main.h"
+#include <stdio.h>
 /**
-* main - 
-* Return: 
+*print_number - prints an integer
+* @n: the integer to be printed
+* Return: nothing
 */
+void print_number(int n)
+{
+unsigned int o = n;
+if (n < 0)
+{
+n *= -1;
+o = n;
+_putchar('-');
+}
+o /= 10;
+if (o != 0)
+{
+print_number(o);
+_putchar((unsigned int) n % 10 + '0');
+}
